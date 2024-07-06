a1b1e26e2dddd92c6a2e5935bbc54d077209a035
cyoon1729 <cjy2129@columbia.edu>
2024-07-06 02:58:18 -0400

    format

--- src/Blog/Generate.hs
+++ src/Blog/Generate.hs
@@ -9,33 +9,34 @@ createBase = Dir.createDirectory "./out"
 
 createSubDirectories :: IO [()]
 createSubDirectories = mapM Dir.createDirectory subDirs
-  where subDirs = ["./out/blog", "./out/projects", "./out/resume", "./out/files"]
+  where
+    subDirs = ["./out/blog", "./out/projects", "./out/resume", "./out/files"]
 
 createHome :: IO ()
 createHome = do
-  page <- generatePageStr "static/pages/home.md"
-  writeFile "out/index.html" page
+    page <- generatePageStr "static/pages/home.md"
+    writeFile "out/index.html" page
 
 createResumePage :: IO ()
 createResumePage = do
-  page <- generatePageStr "static/pages/resume.md"
-  writeFile "out/resume/index.html" page
+    page <- generatePageStr "static/pages/resume.md"
+    writeFile "out/resume/index.html" page
 
 createBlogHome :: IO ()
 createBlogHome = do
-  page <- generatePageStr "static/pages/blog.md"
-  writeFile "out/blog/index.html" page
+    page <- generatePageStr "static/pages/blog.md"
+    writeFile "out/blog/index.html" page
 
 createProjectsHome :: IO ()
 createProjectsHome = do
-  page <- generatePageStr "static/pages/projects.md"
-  writeFile "out/projects/index.html.html" page
+    page <- generatePageStr "static/pages/projects.md"
+    writeFile "out/projects/index.html.html" page
 
 generateAll :: IO ()
 generateAll = do
-  createBase
-  _ <- createSubDirectories
-  createHome
-  createResumePage
-  createBlogHome
-  createProjectsHome
+    createBase
+    _ <- createSubDirectories
+    createHome
+    createResumePage
+    createBlogHome
+    createProjectsHome
--- src/MyLib.hs
+++ src/MyLib.hs
@@ -1,4 +1,4 @@
 module MyLib (serve, generateAll) where
 
-import Blog.Server (serve)
 import Blog.Generate (generateAll)
+import Blog.Server (serve)

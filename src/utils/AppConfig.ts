209ebbbc0837ec009240653f3c85074188e90663
Chris Yoon <cjy2129@columbia.edu>
2024-07-06 16:36:28 +0900

    burn everything reach with fire

--- src/utils/AppConfig.ts
+++
@@ -1,9 +0,0 @@
-export const AppConfig = {
-  site_name: 'Chris Yoon',
-  title: 'Chris Yoon',
-  description: 'Chris Yoon',
-  url: 'https://example.com',
-  locale: 'en',
-  author: 'Chris Yoon',
-  pagination_size: 5,
-};
